6a6b41daf4210a609f64693c97086b918ba43662
priyabratadas <pdas544@gmail.com>
2026-04-26 22:17:56 +0530

    dynamic link update. add not found page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from "./pages/HomePage";
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
             <Route path="/jobs" element={<JobsPage />} />
+            <Route path="*" element={<NotFoundPage/>} />
         </Route>
       </Routes>
    
