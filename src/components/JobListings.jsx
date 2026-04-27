fe56f37480f9dfa0e595e58c308e5a6afd3db003
priyabratadas <pdas544@gmail.com>
2026-04-27 20:42:43 +0530

    add json-server package. fetch and display data from json-server using api.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,28 +1,64 @@
 import React from 'react'
-import jobs from '../jobs.json'
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
 
 const JobListings = ({isHome = false}) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  //when this component loads, initially the array is empty
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //useEffect to fetch the data from the json file when the component mounts
+  // and set the state with the fetched data
+  //initially it is empty
+  useEffect(() => {
+    //fetch the data using async
+    const fetchJobs = async () => { 
+      const apiURL = isHome ? 
+      '/api/jobs?_page=1&_per_page=3' 
+      : '/api/jobs';
+      try{
+        const res = await fetch(apiURL);
+        const data = await res.json();
+
+        const jobsData = Array.isArray(data) ? data : data.data || [];
+        setJobs(jobsData);
+      }
+      catch(error){
+        console.log("Error fetching data", error);
+
+      }finally{
+        setLoading(false);
+      }
+    }
+    fetchJobs();
+  },[isHome]);
+
   return (
   
     
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Latest Job Listings" : "All Job Listings"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {
-            jobListings.map((job)=>(
-                <JobListing key={job.id} job={job}/>
-                
-            ))
-            
-          }
+          { loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <>
+            {
+              jobs.map((job)=>(
+                  <JobListing key={job.id} job={job}/>
+                  
+              ))
+              
+            }
+            </>
+        )}
           
         
-        
+  
         </div>
       </div>
     </section>
