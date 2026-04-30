50181915fbc21b58887485b86189294a2e61f018
priyabratadas <pdas544@gmail.com>
2026-04-30 10:37:56 +0530

    replace BrowserRouter. add single job-page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import {Routes, Route} from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from "./pages/HomePage";
 import JobsPage from './pages/JobsPage';
+import JobPage, {jobLoader} from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="jobs" element={<JobsPage />} />
+      <Route path="jobs/:id" element={<JobPage />} loader={jobLoader} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 
 function App() {
- 
-
   return (
-    <>
-      
-    <Routes>
-        <Route path="/" element={<MainLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/jobs" element={<JobsPage />} />
-            <Route path="*" element={<NotFoundPage/>} />
-        </Route>
-      </Routes>
-   
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import React, { use } from 'react'
 import { useState } from 'react'
 import {FaMapMarker} from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 const JobListing = ({job}) => {
   const [showMore, setShowMore] = useState(false);
@@ -37,12 +38,12 @@ const JobListing = ({job}) => {
                   <FaMapMarker className="inline text-lg mr-1 mb-1" />
                   {job.location}
                 </div>
-                <a
-                  href="job.html"
+                <Link
+                  to={`/jobs/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
-                </a>
+                </Link>
               </div>
             </div>
           </div>
